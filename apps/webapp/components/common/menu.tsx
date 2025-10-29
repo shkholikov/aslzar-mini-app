210715721f836c997beae5f5fa7f06129b65fb57
Shakhzod Kholikov <51167612+shkholikov@users.noreply.github.com>
2025-10-29 18:22:10 +0100

    Update menu.tsx

--- apps/webapp/components/common/menu.tsx
+++ apps/webapp/components/common/menu.tsx
@@ -2,13 +2,18 @@
 import { navigationItems } from "@/lib/navigation";
 import { useRouter } from "next/navigation";
 import { MenuDock } from "../ui/shadcn-io/menu-dock";
+import { useTelegram } from "@/hooks/useTelegram";
 
 export function Menu() {
 	const router = useRouter();
+    const tg = useTelegram();
 
 	const navItems = navigationItems.map((item) => ({
 		...item,
-		onClick: () => router.push(item.path)
+		onClick: () => {
+          tg?.HapticFeedback?.impactOccurred("light");
+          router.push(item.path);
+      }
 	}));
 	return (
 		<div className="flex items-end justify-center min-h-[120px] fixed bottom-6">
