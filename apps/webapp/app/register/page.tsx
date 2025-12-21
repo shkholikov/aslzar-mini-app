ba5054b4d190829158dad16326ba13d656edf8c4
shkholikov <imshakhzod@gmail.com>
2025-12-21 03:49:56 +0100

    Update registration page styles: Change border radius to 3xl for improved visual design and consistency.

--- apps/webapp/app/register/page.tsx
+++ apps/webapp/app/register/page.tsx
@@ -83,7 +83,7 @@ export default function RegisterPage() {
 					<Loading />
 				</div>
 			) : (
-				<div className="m-2 border rounded-lg bg-muted/50 bg-transparent p-4">
+				<div className="m-2 border rounded-3xl bg-muted/50 bg-transparent p-4">
 					<FieldGroup>
 						<FieldSet>
 							<FieldLegend>Ro‘yxatdan o‘tish ma’lumotlari</FieldLegend>
